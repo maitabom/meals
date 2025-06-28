0c9d02b68b8d67ebdd82020a186464447398d407
Fábio Valentim <maitabom@gmail.com>
2025-06-28 19:15:48 -0300

    Atualiza a página de detalhes da refeição para exibir imagem e lista de ingredientes

--- lib/pages/meal.dart
+++ lib/pages/meal.dart
@@ -20,7 +20,48 @@ class MealDetailPage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(title: Text(meal.title), backgroundColor: color),
-      body: Center(child: Text(meal.title)),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(meal.image, fit: BoxFit.cover),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10),
+            child: Text(
+              'Ingredientes',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ),
+          Container(
+            width: 300,
+            height: 200,
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: ListView.builder(
+              itemCount: meal.ingredients.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  color: Theme.of(context).canvasColor,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 10,
+                    ),
+                    child: Text(meal.ingredients[index]),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
