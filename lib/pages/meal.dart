2bd9d28561c992d82586727d85eecfb378a90e07
Fábio Valentim <maitabom@gmail.com>
2025-06-28 19:30:58 -0300

    Refatora a página de detalhes da refeição para usar métodos auxiliares na criação de seções e melhora a estrutura do código

--- lib/pages/meal.dart
+++ lib/pages/meal.dart
@@ -11,6 +11,28 @@ class MealDetailPageArguments {
 class MealDetailPage extends StatelessWidget {
   const MealDetailPage({super.key});
 
+  Widget createSectionTitle(BuildContext context, String title) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Text(title, style: Theme.of(context).textTheme.titleLarge),
+    );
+  }
+
+  Widget createSectionContainer(BuildContext context, Widget child) {
+    return Container(
+      width: 340,
+      height: 200,
+      padding: EdgeInsets.all(10),
+      margin: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: child,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final args =
@@ -20,47 +42,53 @@ class MealDetailPage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(title: Text(meal.title), backgroundColor: color),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 300,
-            width: double.infinity,
-            child: Image.network(meal.image, fit: BoxFit.cover),
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 10),
-            child: Text(
-              'Ingredientes',
-              style: Theme.of(context).textTheme.titleLarge,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(meal.image, fit: BoxFit.cover),
             ),
-          ),
-          Container(
-            width: 300,
-            height: 200,
-            padding: EdgeInsets.all(10),
-            margin: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(color: Colors.grey),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: ListView.builder(
-              itemCount: meal.ingredients.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  color: Theme.of(context).canvasColor,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 5,
-                      horizontal: 10,
+            createSectionTitle(context, 'Ingredientes'),
+            createSectionContainer(
+              context,
+              ListView.builder(
+                itemCount: meal.ingredients.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    color: Theme.of(context).canvasColor,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 5,
+                        horizontal: 10,
+                      ),
+                      child: Text(meal.ingredients[index]),
                     ),
-                    child: Text(meal.ingredients[index]),
-                  ),
-                );
-              },
+                  );
+                },
+              ),
             ),
-          ),
-        ],
+            createSectionTitle(context, 'Modo de Preparo'),
+            createSectionContainer(
+              context,
+              ListView.builder(
+                itemCount: meal.steps.length,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      ListTile(
+                        leading: CircleAvatar(child: Text('${index + 1} ')),
+                        title: Text(meal.steps[index]),
+                      ),
+                      Divider(),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
