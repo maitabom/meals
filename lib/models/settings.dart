28d46784332e90749bf858c40f4a1257d0fee4ac
Fábio Valentim <maitabom@gmail.com>
2025-06-29 20:03:25 -0300

    Adiciona botão flutuante na página de detalhes da refeição e implementa a lógica de navegação na página de configurações com opções de filtro

---
+++ lib/models/settings.dart
@@ -0,0 +1,13 @@
+class Settings {
+  bool glutenFree;
+  bool lactoseFree;
+  bool vegan;
+  bool vegetarian;
+
+  Settings({
+    this.glutenFree = false,
+    this.lactoseFree = false,
+    this.vegan = false,
+    this.vegetarian = false,
+  });
+}
--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,15 +1,85 @@
 import 'package:flutter/material.dart';
 import 'package:meals/components/main_drawer.dart';
+import 'package:meals/models/settings.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  var settings = Settings();
+
+  Widget createdSwitch(
+    String title,
+    String subtitle,
+    bool checked,
+    ValueChanged<bool> onChanged,
+  ) {
+    return SwitchListTile.adaptive(
+      title: Text(title),
+      subtitle: Text(subtitle),
+      value: checked,
+      onChanged: onChanged,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Configurações')),
       drawer: MainDrawer(),
-      body: Center(child: Text('Configurações')),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Configurações',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                createdSwitch(
+                  'Sem Glúten',
+                  'Exibir apenas as refeições sem glútem',
+                  settings.glutenFree,
+                  (value) => setState(() {
+                    settings.glutenFree = value;
+                  }),
+                ),
+                createdSwitch(
+                  'Sem Lactose',
+                  'Exibir apenas as refeições sem lactose',
+                  settings.lactoseFree,
+                  (value) => setState(() {
+                    settings.lactoseFree = value;
+                  }),
+                ),
+                createdSwitch(
+                  'Vegana',
+                  'Exibir apenas as refeições veganas',
+                  settings.vegan,
+                  (value) => setState(() {
+                    settings.vegan = value;
+                  }),
+                ),
+                createdSwitch(
+                  'Vegetariana',
+                  'Exibir apenas as refeições vegetarianas',
+                  settings.vegetarian,
+                  (value) => setState(() {
+                    settings.vegetarian = value;
+                  }),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
